5a4dee65ebe234af4302213c6c304600a63ae997
Jason Lengstorf <jason@lengstorf.com>
2024-10-03 13:11:19 -0700

    fix: disable cron to save bandwidth

--- convex/crons.ts
+++ convex/crons.ts
@@ -3,10 +3,12 @@ import { internal } from './_generated/api';
 
 const crons = cronJobs();
 
-crons.interval(
-	'Random chance of spoop',
-	{ seconds: 1 },
-	internal.ideas.randomizeSpookiness,
-);
+// NOTE: I turned this off because it’s resource intensive
+// and I was going to hit bandwidth limits
+// crons.interval(
+// 	'Random chance of spoop',
+// 	{ seconds: 1 },
+// 	internal.ideas.randomizeSpookiness,
+// );
 
 export default crons;
